c5529b70518a843ec499eb15c7f04c3f3295c4b6
klementtan <klementtan@gmail.com>
2020-06-14 03:03:34 +0800

    Update init script

--- init_cron.sh
+++ init_cron.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-cwd=$(pwd)
+if [ -x "$(command -v virtualenv)" ]; then
+  virtualenv venv
+else if [ -x "$(command -v pip3)" ]; then
+  sudo pip3 install virtualenv
+  virtualenv venv 
+else 
+  sudo pip install virtualenv
+  virtualenv venv  
+fi
 
-(crontab -l 2>/dev/null; echo "0 18 * * * cd ${cwd} && . venv/bin/activate && $(which python3) main.py >> ~/cron.log && deactivate") | crontab -
+
+cwd=$(pwd)
+(crontab -l 2>/dev/null; echo "0 18 * * * cd ${cwd} && " \
+". venv/bin/activate && " \
+"$(which python3) main.py >> ~/cron.log && "\
+"deactivate") | crontab -
