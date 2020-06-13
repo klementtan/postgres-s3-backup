b93287b62bb6fd209bf7325e66fc7b3e7d1a7e41
klementtan <klementtan@gmail.com>
2020-06-14 03:23:28 +0800

    Update init script

--- init_cron.sh
+++ init_cron.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+rm -rf venv
+if [ -x "$(command -v virtualenv)" ]; then
+  virtualenv venv
+  soucre venv/bin/activate
+elif [ -x "$(command -v pip3)" ]; then
+  sudo pip3 install virtualenv
+  virtualenv venv
+  souce venv/bin/activate
+else
+  sudo pip install virtualenv
+  virtualenv venv
+  souce ./venv/bin/activate
+fi
+
+if [ -x "$(command -v pip3)" ]; then
+  pip3 install -r requirements.txt
+else
+  pip install -r requirements.txte
+fi
+
+
 cwd=$(pwd)
 
 (crontab -l 2>/dev/null; echo "0 18 * * * cd ${cwd} &&" \
