2a0fae63d693fee204e0b160338e19355153267f
klementtan <klementtan@gmail.com>
2020-06-14 03:04:47 +0800

    Update init script

--- init_cron.sh
+++ init_cron.sh
@@ -12,7 +12,4 @@ fi
 
 
 cwd=$(pwd)
-(crontab -l 2>/dev/null; echo "0 18 * * * cd ${cwd} && " \
-". venv/bin/activate && " \
-"$(which python3) main.py >> ~/cron.log && "\
-"deactivate") | crontab -
+(crontab -l 2>/dev/null; echo "0 2 * * * cd ${cwd} && . venv/bin/activate && $(which python3) main.py >> ~/cron.log && deactivate") | crontab -
